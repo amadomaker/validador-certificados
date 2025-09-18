2bb10931952ce7f0861d36561566c16611f5b2ed
vinicius3516 <costavinicius692@gmail.com>
2025-09-18 08:51:15 -0300

    Update API_URL in config.js for clarity and consistency

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,2 @@
-// Centraliza variáveis de configuração do app
-export const API_BASE =
-  'https://script.google.com/macros/s/AKfycbzDO9MuZaXfASS2VgRP3GJcye-i5byJ3rdRlckCqSclFIc8i7EsD3nFA3ZcPp230doN/exec';
+// src/js/config.js
+export const API_URL = 'https://script.google.com/macros/s/AKfycbzDO9MuZaXfASS2VgRP3GJcye-i5byJ3rdRlckCqSclFIc8i7EsD3nFA3ZcPp230doN/exec';
